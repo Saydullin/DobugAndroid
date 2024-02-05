6f9d7ec874f90692444f3db883262d915f46bdad
Saydullin <saydullindev@gmail.com>
2024-02-06 01:36:44 +0300

    Init commit

--- app/src/main/java/com/saydullin/codehub/di/DataModule.kt
+++ app/src/main/java/com/saydullin/codehub/di/DataModule.kt
@@ -1,13 +1,52 @@
 package com.saydullin.codehub.di
 
+import android.content.Context
+import androidx.room.Room
+import com.saydullin.codehub.data.db.AppDatabase
+import com.saydullin.codehub.data.db.dao.ArticleDao
+import com.saydullin.codehub.data.repository.ArticleRepositoryImpl
+import com.saydullin.codehub.domain.repository.ArticleRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
-@InstallIn(SingletonComponent::class)
 @Module
+@InstallIn(SingletonComponent::class)
 class DataModule {
 
+    @Provides
+    @Singleton
+    fun provideAppDatabase(
+        @ApplicationContext context: Context
+    ): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "CodeHubDatabase"
+        )
+            .allowMainThreadQueries()
+            .build()
+    }
 
+    @Provides
+    @Singleton
+    fun provideArticleDao(appDatabase: AppDatabase): ArticleDao {
+        return appDatabase.articleDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideArticleRepository(
+        articleDao: ArticleDao
+    ): ArticleRepository {
+        return ArticleRepositoryImpl(
+            articleDao = articleDao,
+        )
+    }
 
 }
+
+
--- app/src/main/java/com/saydullin/codehub/di/DomainModule.kt
+++ app/src/main/java/com/saydullin/codehub/di/DomainModule.kt
@@ -1,8 +1,11 @@
 package com.saydullin.codehub.di
 
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 
 @Module
+@InstallIn(ViewModelComponent::class)
 class DomainModule {
 
 
